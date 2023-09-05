e8ef2e3802f5b17774d874da000494d1cef45c89
aeebbr <aeebbr@gmail.com>
2023-09-05 15:28:52 +0900

    boj_1600_말이되고픈원숭이 주석 추가

--- 09/week01/boj_1600_말이되고픈원숭이.py
+++ 09/week01/boj_1600_말이되고픈원숭이.py
@@ -10,13 +10,14 @@ def bfs(sr, sc):
     # 큐 초기화 
     q = deque()
     # 방문배열 초기화 
-    # 0 ~ k까지 
+    # 0 ~ k까지 말 이동 횟수에 따라서 경로 종류를 나눔
     visited = [[[0] * (k+1) for _ in range(W)] for _ in range(H)]
 
     q.append((sr, sc, 0))
     visited[sr][sc][0] = 1
 
     while q:
+        # horse: 현재 지점까지의 말 이동 횟수 
         cr, cc, horse = q.popleft()
 
         if cr == H - 1 and cc == W - 1:
@@ -28,8 +29,8 @@ def bfs(sr, sc):
             nh = horse
 
             if 0 <= dir <= 7:
-                # horse > k로 하면 말 증가할 수 없음 
-                if horse >= k:
+                # 말 이동 횟수가 최대치라면 말 이동 불가 
+                if horse == k:
                     continue
                 nh += 1
 
@@ -38,7 +39,12 @@ def bfs(sr, sc):
             if 0 <= nr < H and 0 <= nc < W and not visited[nr][nc][nh]:
                 # 장애물 여부 
                 if not board[nr][nc]:
+                    # 말 이동 횟수에 따른 경로를 삽입
                     q.append((nr, nc, nh))
+                    # 말 이동 횟수에 따른 경로에 카운트를 삽입하여 방문 처리 
+                    # 이번 턴에서 말 이동했다면 경로가 달라지는 것임
+                    # ex. 현재지점까지는 '1회 이동' 경로로 왔는데 이번 턴에서 '2회 이동' 경로로 전환해야 함
+                    # => '1회 이동' 경로에 지금까지의 카운트가 저장되어 있으니, 거기에서 1증가하여 '2회 이동'에 저장 
                     visited[nr][nc][nh] = visited[cr][cc][horse]+1
 
     return -1
