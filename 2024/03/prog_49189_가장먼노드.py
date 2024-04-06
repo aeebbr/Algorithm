e464ab2337faaa5d3fe7eab1d221bf2266cb3536
aeebbr <aeebbr@gmail.com>
2024-04-06 19:47:30 +0900

    prog_49189_가장먼노드 풀이

---
+++ 2024/03/prog_49189_가장먼노드.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+def bfs(n, edge, linked_list):
+    q = deque()
+    q.append((1, 0))
+    visited = [False] * (n+1)
+    visited[1] = True
+    answer = [0] * (n+1)
+    
+    while q:
+        cur, cnt = q.popleft()
+        
+        if answer[cur] == 0:
+            answer[cur] = cnt
+        
+        for i in range(1, len(linked_list[cur])):
+            next = linked_list[cur][i]
+            
+            if not visited[next]:
+                q.append((next, cnt + 1))
+                visited[next] = True
+                
+    return answer.count(max(answer))
+        
+def solution(n, edge):
+    linked_list = [[0] for _ in range(n+1)]
+    
+    for a, b in edge:
+        linked_list[a].append(b)
+        linked_list[b].append(a)
+    
+    return bfs(n, edge, linked_list)
+    
